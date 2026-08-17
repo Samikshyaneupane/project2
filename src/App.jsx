b887493449e9fdb254e0ba49a086a34f6a03e2fc
Samikshyaneupane <samikshyaneupane87@gmail.com>
2026-08-17 21:06:46 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './components/card'
+import Navbar from './components/navbar'
+const App = () =>{
+    return(
+   <div>
+    <Card/>
+    <Navbar />
+   </div>
+    )
+}
+export default App
---
+++ src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className='card'>
+      <h1>Samikshya Neupane</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, magni!</p>
+    </div>
+  )
+}
+
+export default card
